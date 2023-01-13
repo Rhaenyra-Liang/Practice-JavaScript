8f458091e2554a5117045a8c0fc7cac07a114b31
Rhaenyra Liang <sofialiang19991029@gmail.com>
2023-01-13 10:44:13 +0800

    practice 4

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-window.onload = function () {
-  document.getElementById("count-0").innerText = 5;
-};
+// window.onload = function () {
+//   document.getElementById("count-0").innerText = 5;
+// };
+
+let countl = 0;
+
+console.log(count);
+
+let count = 50;
+count = count + 50;
+count = count - 75;
+count = count + 45;
+console.log(count);
