5b23c971aa6a019b6cb2ff16aa79611114f2f054
Rhaenyra Liang <sofialiang19991029@gmail.com>
2023-01-15 16:03:06 +0800

    practice 7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // window.onload = function () {
-//   document.querySelector("count-el").innerText = 5;
+//   document.querySelector("count-el"). = 5;
 // };
 
 // intialize the count as 0
@@ -8,15 +8,22 @@
 // change the count-el in HTML to reflect the new count
 
 let countElement = document.querySelector("#count-el");
+let saveElement = document.querySelector("#save-el");
 let count = 0;
 
 function increment() {
   count += 1;
-  countElement.innerText = count;
+  countElement.textContent = count;
 }
 
 // 1. Creat a function, save(), which logs out the count when it's call
+// 2. Creat a variable that contains both the count and the dash separator
+// 3. Render the variable in the saveEL usng innerText
+// Make sure to not delete the existing content of the paragraph
 
 function save() {
-  console.log(count);
+  let countString = count + " - ";
+  saveElement.textContent += countString;
+  countElement.textContent = 0;
+  count = 0;
 }
