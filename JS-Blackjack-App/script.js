997bb00457ff53e65c684dd20e0312fd7212a92a
Rhaenyra Liang <sofialiang19991029@gmail.com>
2023-01-22 15:57:06 +0800

    update5

--- JS-Blackjack-App/script.js
+++ JS-Blackjack-App/script.js
@@ -1,5 +1,6 @@
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard]; // array ordered list of item
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -8,8 +9,15 @@ let messageElement = document.querySelector("#message-el");
 let sumElement = document.querySelector("#sum-el");
 let cardElement = document.querySelector("#card-el");
 
+// Creat  a new function called startGame() that calls rendergame()
 function startGame() {
-  cardElement.textContent = "Card: " + firstCard + " " + secondCard;
+  renderGame();
+}
+
+function renderGame() {
+  // render out firstcCard and secondCard
+  cardElement.textContent = "Cards: " + cards[0] + " " + cards[1];
+  // render out All the card we havw
   sumElement.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card? 😌";
@@ -29,5 +37,5 @@ function newCard() {
   // 2. Add the new card to the sum variable
   sum += card;
   // 3. call startgame()
-  startGame();
+  renderGame();
 }
